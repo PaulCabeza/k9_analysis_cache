9519598d750a278282dfac40442026b72f737358
Paul Cabeza <ovidio.cabeza@gmail.com>
2025-01-04 11:56:08 -0600

    Add: AnalysisCacheEntry model

--- cachemanager/models.py
+++ cachemanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class AnalysisCacheEntry(models.Model):
+    id = models.CharField(max_length=127, unique=True, primary_key=True)
+    partition_id = models.CharField(max_length=30)
+    data = models.JSONField(null=True, blank=True)
+    valid_until = models.BigIntegerField(null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['partition_id'])
+        ]
